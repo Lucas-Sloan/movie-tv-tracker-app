cafced1ce5128b16a07e157763205d4f6e084431
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-08-01 16:11:02 -0500

    Added Schema, made directories for pages

--- models/user.js
+++ models/user.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+const cinemaSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    score: { type: Number, required: true, min: 0, max: 10 },
+    status: { type: String, enum: ["Completed", "Haven't Completed", "Plan to Watch"], required: true }, // Required
+    description: { type: String },
+    type: { type: String, enum: ["Movie", "TV Show"], required: true },
+    api_key: { type: String }
 });
 
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Cinema' }], 
+    tv_shows: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Cinema' }] 
+});
+
+const Cinema = mongoose.model('Cinema', cinemaSchema);
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+module.exports = { Cinema, User };
