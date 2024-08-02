ddf125a00f75d0427c63a0e96fbbc477d7597af0
Lucas-Sloan <Lucasg.sloan@gmail.com>
2024-08-02 09:10:04 -0500

    Added new TV show page, it inputs and saves to the dashboard.

--- controllers/cinemas.js
+++ controllers/cinemas.js
@@ -10,9 +10,6 @@ router.get('/dashboard', isSignedIn, async (req, res) => {
     const movies = await Cinema.find({ user: userId, type: 'Movie' });
     const tvshows = await Cinema.find({ user: userId, type: 'TV Show' });
 
-    console.log("Movies:", movies); // Log movies to console
-    console.log("TV Shows:", tvshows); // Log TV shows to console
-
     res.render('cinemas/dashboard.ejs', {
       user: req.session.user,
       movies,
@@ -39,6 +36,28 @@ router.post('/newmovie', isSignedIn, async (req, res) => {
       type: 'Movie'
     });
     await newMovie.save();
+    res.redirect('/cinemas/dashboard');
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.get('/newtv', isSignedIn, (req, res) => {
+  res.render('cinemas/newtv.ejs');
+});
+
+router.post('/newtv', isSignedIn, async (req, res) => {
+  try {
+    const newTVShow = new Cinema({
+      user: req.session.user._id,
+      title: req.body.title,
+      score: req.body.score,
+      status: req.body.status,
+      description: req.body.description,
+      type: 'TV Show'
+    });
+    await newTVShow.save();
     res.redirect('/cinemas/dashboard');
   } catch (error) {
     console.log(error);
